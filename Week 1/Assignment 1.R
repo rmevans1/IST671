646c2c16b86fc755a6268b1f0850a3dca0c3559c
Robert Evans <robertevans@robertevansmb.com>
2017-03-20 15:23:08 -0400

    Finished Chapter 2 of assignment 1 coding

--- Week 1/Assignment 1.R
+++ Week 1/Assignment 1.R
@@ -1,5 +1,6 @@
 str(iris)
 
+#Chapter 1
 #Commenting the install.packages out to prevent installation from running multiple times
 #install.packages("mboost")
 library(mboost)
@@ -8,3 +9,22 @@ library(mboost)
 #data("bodyfat", package="mboost")
 data("bodyfat", package = "TH.data")
 str(bodyfat)
+
+#Chapter 2- Data Import/Exmport
+
+#Export/Import to Rdata
+a <- 1:10
+save(a, file="data/dumData.Rdata")
+rm(a)
+load("data/dumData.Rdata")
+print(a)
+
+#Export/Import to CSV
+var1 <- 1:5
+var2 <- (1:5) / 10
+var3 <- c("R", "and", "Data Mining", "Examples", "Case Studies")
+a <- data.frame(var1, var2, var3)
+names(a) <- c("VariableInt", "VariableReal", "VariableChar")
+write.csv(a, "data/dummyData.csv", row.names = FALSE)
+b <- read.csv("data/dummyData.csv")
+print(b)
