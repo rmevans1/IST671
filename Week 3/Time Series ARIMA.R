8d4f439f5ebaec1b14734a16b1a68bf2b142814d
Robert Evans <robertevans@robertevansmb.com>
2017-04-01 20:43:49 -0400

    Finished arima tutorial

--- Week 3/Time Series ARIMA.R
+++ Week 3/Time Series ARIMA.R
@@ -39,3 +39,44 @@ pacf(d.Y)
 
 # ARIMA(1.0.0) or AR(1)
 arima(Y, order=c(1,0,0))
+
+# ARIMA(2.0.0) or AR(2)
+arima(Y, order=c(2,0,0))
+
+# ARIMA(0.0.1) or MA(1)
+arima(Y, order=c(0,0,1))
+
+# ARIMA(1.0.1) or AR(1) MA(1)
+arima(Y, order = c(1,0,1))
+
+# ARIMA on differenced variable
+# ARIMA(1.0.0)
+arima(d.Y, order=c(1,0,0))
+
+# ARIMA(0.1.1)
+arima(d.Y, order=c(0,0,1))
+
+# ARIMA(1.1.1) 
+arima(d.Y, order=c(1,0,1))
+
+# ARIMA(1.1.3)
+arima(d.Y, order = c(1,0,3))
+
+# ARIMA(2.1.3)
+arima(d.Y, order = c(2,0,3))
+
+# ARIMA(1,0,1) forecasting
+mydata.arima101 <- arima(Y, order = c(1,0,1))
+mydata.pred1 <- predict(mydata.arima101, n.ahead=100)
+plot (Y)
+lines(mydata.pred1$pred, col="blue")
+lines(mydata.pred1$pred+2*mydata.pred1$se, col="red")
+lines(mydata.pred1$pred-2*mydata.pred1$se, col="red")
+
+# ARIMA(1,1,1) forecasting
+mydata.arima111 <- arima(d.Y, order = c(1,0,1))
+mydata.pred1 <- predict(mydata.arima111, n.ahead=100)
+plot (d.Y)
+lines(mydata.pred1$pred, col="blue")
+lines(mydata.pred1$pred+2*mydata.pred1$se, col="red")
+lines(mydata.pred1$pred-2*mydata.pred1$se, col="red")
